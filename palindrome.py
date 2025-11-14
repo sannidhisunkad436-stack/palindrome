738e2313da63d4847039cfda57a8bc11a849af67
Sannidhi.Sunkad <01fe24ica004@kletech.ac.in>
2025-11-14 10:55:20 +0530

    initial palindrome modified

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,10 @@
 # check if the given string is palindrome or not
-def is_palindrome(s):
-    if len(s) < 4:
-        raise ValueError("Input must have at least 4 characters.")
-    return s == s[::-1]
+s = input("Enter a string: ")
 
-if __name__ == "__main__":
-    user_input = input("Enter a string: ")
-    try:
-        result = is_palindrome(user_input)
-        print(result)
-    except ValueError as e:
-        print("Error:", e)
+if len(s) < 4:
+    print("Error: Input must have at least 4 characters.")
+else:
+    if s == s[::-1]:
+        print(True)
+    else:
+        print(False)

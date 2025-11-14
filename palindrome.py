89aaf0e018397b8899c1c51e8072ba85e21eb8a6
Sannidhi.Sunkad <01fe24ica004@kletech.ac.in>
2025-11-14 11:24:29 +0530

    initial palindrome modified

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,25 @@
-# check if the given string is palindrome or not
-s = input("Enter a string: ")
+import sys
 
-if len(s) < 4:
-    print("Error: Input must have at least 4 characters.")
+print("=== Palindrome Checker ===")
+
+# Take input from CLI or user
+if len(sys.argv) > 1:
+    text = " ".join(sys.argv[1:])
 else:
-    if s == s[::-1]:
-        print(True)
-    else:
-        print(False)
+    text = input("Enter a string: ")
+
+# Count alphabetic letters only
+letters = "".join(ch.lower() for ch in text if ch.isalpha())
+
+# Check minimum length
+if len(letters) < 4:
+    print("Error: Please enter at least 4 letters.")
+    print("Letters found:", len(letters))
+    exit()
+
+# Palindrome check
+if letters == letters[::-1]:
+    print("Yes, it is a palindrome.")
+else:
+    print("No, it is not a palindrome.")
+
